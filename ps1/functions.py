5511053386b31e1c218cf8291a9975dc08116d46
varun <varun4socials@gmail.com>
2026-07-09 09:09:52 +0530

    Initial commit

---
+++ ps1/functions.py
@@ -0,0 +1,106 @@
+DOMAIN = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+a = ord('a')
+z = ord('z')
+
+def additive_cipher_encrypt(text, key):
+    encrypted = ""
+    
+    for ch in text.lower():
+        character = ord(ch)
+        if character >= a and character <= z:
+            index = a + (character + key - a) % 26
+            encrypted += chr(index)
+        else:
+            encrypted += ch
+    
+    return encrypted
+
+
+def additive_cipher_decrypt(text, key):
+    decrypted = ""
+    
+    for ch in text:
+        character = ord(ch)
+        if character >= a and character <= z:
+            index = a + (character - key - a) % 26
+            decrypted += chr(index)
+        else:
+            decrypted += ch
+    
+    return decrypted
+
+
+def multiplicative_cipher_encrypt(text, key):
+    if 26 % key == 0 or key < 1 or key > 25:
+        return "invalid key"
+    
+    encrypted = ""
+    
+    for ch in text.lower():
+        character = ord(ch)
+        if character >= a and character <= z:
+            index = a + ((character - a) * key) % 26
+            encrypted += chr(index)
+        else:
+            encrypted += ch
+            
+    return encrypted
+
+
+def get_multiplicative_inverse(key):
+    for i in DOMAIN:
+        if key * i % 26 == 1:
+            return i
+    return None
+
+
+def multiplicative_cipher_decrypt(text, key):
+    decrypted = ""
+    
+    inverse_key = get_multiplicative_inverse(key)
+
+    if inverse_key is None:
+        return "invalid key"
+    
+    for ch in text:
+        character = ord(ch)
+        if character >= a and character <= z:
+            index = a + ((character - a) * inverse_key) % 26
+            decrypted += chr(index)
+        else:
+            decrypted += ch
+    
+    return decrypted
+
+
+def affine_cipher_encrypt(text, k1, k2):
+    encrypted = ""
+    
+    for ch in text.lower():
+        character = ord(ch)
+        if character >= a and character <= z:
+            index = a + ((character - a) * k1 + k2) % 26
+            encrypted += chr(index)
+        else:
+            encrypted += ch
+            
+    return encrypted
+
+
+def affine_cipher_decrypt(text, k1, k2):
+    decrypted = ""
+    
+    k1_inverse = get_multiplicative_inverse(k1)
+
+    if k1_inverse is None:
+        return "invalid key"
+    
+    for ch in text:
+        character = ord(ch)
+        if character >= a and character <= z:
+            index = a + (((character - k2 - a) * k1_inverse) % 26)
+            decrypted += chr(index)
+        else:
+            decrypted += ch
+            
+    return decrypted
---
+++ ps1/main.py
@@ -0,0 +1,30 @@
+from functions import (
+    additive_cipher_encrypt,
+    additive_cipher_decrypt,
+    multiplicative_cipher_encrypt,
+    multiplicative_cipher_decrypt,
+    affine_cipher_encrypt,
+    affine_cipher_decrypt
+)
+
+text = "this is an exercise"
+ace = additive_cipher_encrypt(text, 20)
+acd = additive_cipher_decrypt(ace, 20)
+
+mce = multiplicative_cipher_encrypt(text, 15)
+mcd = multiplicative_cipher_decrypt(mce, 15)
+
+affce = affine_cipher_encrypt(text, 15, 20)
+affcd = affine_cipher_decrypt(affce, 15, 20)
+
+print("Additive Cipher")
+print(f"Encrypt({text}) = ", ace)
+print(f"Decrypt({ace}) = ", acd)
+
+print("Multiplicative Cipher")
+print(f"Encrypt({text}) = ", mce)
+print(f"Decrypt({mce}) = ", mcd)
+
+print("Affine Cipher")
+print(f"Encrypt({text}) = ", affce)
+print(f"Decrypt({affce}) = ", affcd)
